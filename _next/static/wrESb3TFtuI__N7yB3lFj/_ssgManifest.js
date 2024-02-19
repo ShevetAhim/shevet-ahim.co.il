e94344ee3a85e1964732ad03bf794183030b0454
Nagasaki45 <nagasaki45@gmail.com>
2024-02-19 20:28:15 +0000

    Deploying to gh-pages from @ ShevetAhim/shevet-ahim.co.il@405d90801177a0c067259825d0c73bf3e4a80ba4 🚀

--- _next/static/wrESb3TFtuI__N7yB3lFj/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Factivities","\u002Fblog","\u002F","\u002Fnews","\u002Fabout","\u002Fcontact","\u002Femail_sent","\u002F[category]\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
